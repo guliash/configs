c3ef9a8e160d477f808354b322c7d91bf8dabc45
Artem Gilmudinov <guliash92@gmail.com>
2017-11-06 16:57:29 +0300

    paddings;

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,16 @@ module.exports = {
       "experimentalObjectRestSpread": true
     }
   },
-    "extends": "eslint:recommended",
-    "rules": {
-      "no-undef": 0,
-      "no-unused-vars": 0,
-      "no-console": 0,
-      "indent": ["error", 2, { "SwitchCase": 1 }],
-      "max-len": ["error", { "code": 80 }],
-      "no-trailing-spaces": ["error"],
-      "comma-dangle": ["error", "never"],
-      "semi": ["error", "always"],
-      "object-curly-spacing": ["error", "always"]
-    }
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-undef": 0,
+    "no-unused-vars": 0,
+    "no-console": 0,
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "max-len": ["error", { "code": 80 }],
+    "no-trailing-spaces": ["error"],
+    "comma-dangle": ["error", "never"],
+    "semi": ["error", "always"],
+    "object-curly-spacing": ["error", "always"]
+  }
 };
